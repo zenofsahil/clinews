9aeb00b33223ad8a5d8b6534dd5cf1bc259810c3
Sahil <sahilsingh90@gmail.com>
2022-06-14 04:35:20 +0530

    Add newsapi to headlines app

--- headlines/src/main.rs
+++ headlines/src/main.rs
@@ -1,6 +1,20 @@
 mod headlines;
 
-use headlines::Headlines;
+use headlines::{ Headlines, NewsCardData };
+use newsapi::NewsAPI;
+
+fn fetch_news(api_key: &str) {
+    if let Ok(response) = NewsAPI::new(api_key).fetch() {
+        let articles = response.articles();
+        for a in articles.iter() {
+            let news = NewsCardData {
+                title: a.title().to_string(),
+                url: a.url().to_string(),
+                description: a.description().to_string()
+            };
+        }
+    }
+}
 
 fn main() {
     tracing_subscriber::fmt::init();
