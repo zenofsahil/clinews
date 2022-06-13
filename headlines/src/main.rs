10015e4dafa30f42c08f739b2b4454771540fdb9
Sahil <sahilsingh90@gmail.com>
2022-06-14 04:00:02 +0530

    Init logging

--- headlines/src/main.rs
+++ headlines/src/main.rs
@@ -3,6 +3,7 @@ mod headlines;
 use headlines::Headlines;
 
 fn main() {
+    tracing_subscriber::fmt::init();
     let mut options = eframe::NativeOptions::default();
     options.initial_window_size = Some(eframe::egui::Vec2::new(540., 960.));
     eframe::run_native(
